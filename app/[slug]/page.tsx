d7d1e357a794532a7ddd9f74343cc9c09d8d9a2a
Kyle Karpack <karpack@adobe.com>
2026-02-12 17:17:45 -0800

    More updates

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { getMDXComponents } from "@/app/mdx-components";
-import { ExpertAdvice } from "@/content/charts/expert-advice/ExpertAdvice";
+import { BetterColorScales } from "@/content/charts/better-color-scales/BetterColorScales";
 import { HikeHistogramSmallMultiples } from "@/content/charts/hike-histogram-small-multiples/HikeHistogramSmallMultiples";
 import { HikeHistogram } from "@/content/charts/hike-histogram/HikeHistogram";
 import { INaturalistSunburst } from "@/content/charts/inaturalist-sunburst/INaturalistSunburst";
@@ -33,7 +33,7 @@ export default async function ChartPage({
 
   const components = {
     ...getMDXComponents(),
-    ExpertAdvice,
+    BetterColorScales,
     TuckedTicks,
     Racing,
     TideCharts,
